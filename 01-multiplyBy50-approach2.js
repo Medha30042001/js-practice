0583e862f04b072d56ad0940eed2b85fc5e5ea31
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-04 17:49:08 +0530

    Update and rename 01-multiplyBy50.js to 01-multiplyBy50-approach2.js

--- 01-multiplyBy50-approach2.js
+++ 01-multiplyBy50-approach2.js
@@ -1,12 +1,13 @@
+// Screenshot: multiplyBy50.png
+// My solution (approach 2)
+
 function multiplyBy50(number) {
-    // Write code here
     let ans = number * 50;
     return ans;
 }
 
 function main(){
   const number = require('fs').readFileSync(0, 'utf-8').trim();
-
   console.log(multiplyBy50(parseInt(number)));
 }
 
