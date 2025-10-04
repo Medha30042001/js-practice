9e7e75e5206ffae9aac03b60766434e607122aff
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-04 17:40:47 +0530

    Create 01-multiplyBy50.js

---
+++ 01-multiplyBy50.js
@@ -0,0 +1,13 @@
+function multiplyBy50(number) {
+    // Write code here
+    let ans = number * 50;
+    return ans;
+}
+
+function main(){
+  const number = require('fs').readFileSync(0, 'utf-8').trim();
+
+  console.log(multiplyBy50(parseInt(number)));
+}
+
+main();
