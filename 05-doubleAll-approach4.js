a041b1ae1b5014774c1e91eaf86ba63c6c19d41e
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-06 14:26:49 +0530

    Create 05-doubleAll-approach4.js

---
+++ 05-doubleAll-approach4.js
@@ -0,0 +1,21 @@
+
+
+function doubleAll(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + (arr[i] * 2);
+  }
+
+  return sum;
+}
+
+function main() {
+  const fs = require('fs');
+  const input = fs.readFileSync(0, 'utf-8').trim();
+  const arr = input.split(' ').map(Number); // converts all to numbers
+
+  console.log(doubleAll(arr));
+}
+
+main();
