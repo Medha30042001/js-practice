dc592024221686564108fa8374b46ee158f3c136
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-06 16:53:56 +0530

    Create 14-divisibleBy4.js

---
+++ 14-divisibleBy4.js
@@ -0,0 +1,13 @@
+//my approach
+
+function divisibleBy4(N) {
+    if(N%4==0) return 'Yes'
+    else 'No';
+}
+
+function main(){
+  const N = require('fs').readFileSync(0, 'utf-8').trim();
+  console.log(divisibleBy4(N));
+}
+
+main();
