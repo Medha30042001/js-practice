00d603fa445890a35e74d63caa7d45c657b6c0f9
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-06 13:06:33 +0530

    Create 03-squareIt.js

---
+++ 03-squareIt.js
@@ -0,0 +1,13 @@
+
+
+function squareIt(N) {
+    let ans = N * N;
+    return ans;
+}
+
+function main(){
+  const N = require('fs').readFileSync(0, 'utf-8').trim();
+  console.log(squareIt(parseInt(N)));
+}
+
+main();
