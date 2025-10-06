cc2d55ea206da69c840677311c6e26c613997a67
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-06 14:03:24 +0530

    Create 04-findProduct.js

---
+++ 04-findProduct.js
@@ -0,0 +1,21 @@
+
+
+function findProduct(A,B,C,D,E){
+  let ans = A * B * C * D * E;
+  return ans;
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let arr = input.split(' ');
+  
+  let A = parseInt(arr[0]);
+  let B = parseInt(arr[1]);
+  let C = parseInt(arr[2]);
+  let D = parseInt(arr[3]);
+  let E = parseInt(arr[4]);
+  
+  console.log(findProduct(A,B,C,D,E));
+}
+
+main();
